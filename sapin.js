e1df1c5a5e8b13909485ea0403910fd2815899e2
Fullpotatoes <ludovic.pouet@gmail.com>
2025-01-24 11:57:51 +0100

    push

---
+++ sapin.js
@@ -0,0 +1,33 @@
+// let star = "*";
+// let star2 = "*"
+// let result = star + star;
+// result;
+
+// console.log(result)
+
+const afficherEtoiles = (n) => {
+    let STAR = "*"
+    console.log(STAR.repeat(n))
+}
+
+afficherEtoiles()
+
+const afficherRectangle = (hauteur, largeur) => {
+    let étoiles = afficherEtoiles(largeur)
+    for (let i = 0; i >= hauteur; i++) {
+        console.log(étoiles)
+    }
+}
+
+afficherRectangle()
+
+const afficherTriangleDroite = (n) => {
+    let rightBranch = 
+
+}
+
+afficherTriangleDroite(5)
+
+
+
+
